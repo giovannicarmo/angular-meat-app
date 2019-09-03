47f9b36d30159d62cbe957d960c99ebb73419346
giovannicarmo <giovannicarmo96@gmail.com>
2019-09-02 23:06:43 -0300

    Primeira interação com ngFor (Falta ajustar a exibição)

---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Restaurant } from './restaurant/restaurant.model';
+
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html'
+})
+export class RestaurantsComponent implements OnInit {
+
+  restaurants: Restaurant[] = [
+    {
+      id: "bread-bakery",
+      name: "Bread & Bakery",
+      category: "Bakery",
+      deliveryEstimate: "25m",
+      rating: 4.9,
+      imagePath: "assets/img/restaurants/breadbakery.png",
+    },
+    {
+      id: "burger-house",
+      name: "Burger House",
+      category: "Hamburgers",
+      deliveryEstimate: "100m",
+      rating: 3.5,
+      imagePath: "assets/img/restaurants/burgerhouse.png",
+    },
+    {
+      id: "ice-cream",
+      name: "Ice Cream",
+      category: "Ice Creams",
+      deliveryEstimate: "40-65m",
+      rating: 4.5,
+      imagePath: "assets/img/restaurants/icy.png",
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
